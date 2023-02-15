3b3bc2746c8408fa64b02652509a02d30603cd06
AtiFail <attila.szabo125.work@gmail.com>
2023-02-15 09:22:40 +0100

    opening a picture from my desktop

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
-#include <opencv2/objdetect.hpp>
+#include <opencv2/highgui.hpp>
 
 using namespace std;
 using namespace cv;
 
 int main() {
-    cout << "Hello, World!";
+    cout << "Tesztelés, hogy működik-e!";
+    Mat img = imread("/Users/szaboattila/Desktop/csokis_keksz.jpg");
+    imshow("IMAGE", img);
+    waitKey(0);
     return 0;
 }
