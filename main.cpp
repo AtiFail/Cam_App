bdb14d0b9382a58aaebcf7f011a3cc0580727adb
AtiFail <attila.szabo125.work@gmail.com>
2023-02-15 09:03:53 +0100

    Add opencv libraries and directories in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
