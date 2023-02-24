cba5cbfc5194cecf9f0065b74c87130d6893ff5f
AtiFail <attila.szabo125.work@gmail.com>
2023-02-24 13:53:15 +0100

    camera has access and i can open it

--- main.cpp
+++ main.cpp
@@ -2,20 +2,27 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/objdetect.hpp>
 
 using namespace std;
 using namespace cv;
 
 int main() {
     cout << "Tesztelés, hogy működik-e!";
-    Mat img = imread("/Users/szaboattila/Desktop/csokis_keksz.jpg", IMREAD_GRAYSCALE);
-    imshow("Csokis keksz", img);
-    imwrite("/Users/szaboattila/Desktop/csokis_keksz_szürke.jpg", img);
+    /*Mat img = imread("/Users/szaboattila/Desktop/benya.png");
+    imshow("Face Detection", img);
     waitKey(0);
-    return 0;
+    return 0;*/
 
     //kamera megnyitása
-
+    VideoCapture video(0);
+    Mat img;
+    while (true)
+    {
+        video.read(img);
+        imshow("Video", img);
+        waitKey(20);
+    }
     //objektumfelismerés
 
     //objektumok megszámlálása, majd egy TXT-be menteni az adatokat
