9ceb6af713a1c510a68ce98733139379cea377dd
AtiFail <attila.szabo125.work@gmail.com>
2023-02-24 11:55:34 +0100

    planning the software structure

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
 using namespace std;
@@ -9,8 +8,15 @@ using namespace cv;
 
 int main() {
     cout << "Tesztelés, hogy működik-e!";
-    Mat img = imread("/Users/szaboattila/Desktop/csokis_keksz.jpg");
-    imshow("IMAGE", img);
+    Mat img = imread("/Users/szaboattila/Desktop/csokis_keksz.jpg", IMREAD_GRAYSCALE);
+    imshow("Csokis keksz", img);
+    imwrite("/Users/szaboattila/Desktop/csokis_keksz_szürke.jpg", img);
     waitKey(0);
     return 0;
+
+    //kamera megnyitása
+
+    //objektumfelismerés
+
+    //objektumok megszámlálása, majd egy TXT-be menteni az adatokat
 }
